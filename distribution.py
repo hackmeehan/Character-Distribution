cf81fafe61715bdbbef35cf70a353b852da01911
hackmeehan <42844639+hackmeehan@users.noreply.github.com>
2018-10-09 12:36:49 -0400

    Updated from Brython Server: 10/9/2018 12:36:48 PM

--- distribution.py
+++ distribution.py
@@ -39,9 +39,9 @@ Notice about this example:
 '''
 
 L = input('Please enter a string of text (the bigger the better): ')
-L = L.lower()
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 print('the distribution of characters in "'+L+'" is: ')
+L = L.lower()
 counts = []
 for i in alphabet:
     thiscount = L.count(i)
